ee5b705d31875885800871bb57f1c712bc9e37a8
kilobybro <paddle.spreads-8y@icloud.com>
2026-09-21 17:09:48 +0100

    Create on-sierra.js

---
+++ on-sierra.js
@@ -0,0 +1,5 @@
+var password = 'PHC Recruitment'
+password=prompt('NOTICE: Population and Housing Census (PHC) Recruitment is currently recruiting, press OK to apply for the','PHC Recruitment');
+if (password != 'PHC Recruitment') {
+location.href='https://www.kilobybro.ink/recruit-statistics-sl';
+}
